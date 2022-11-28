4f5e76575961d6bf601aa72723f83a45373db326
Manu <82286334+Manuel-Russo@users.noreply.github.com>
2022-11-28 20:12:43 +0100

    lettura db e configurazione nodeJS

--- Bar_della_fossa/src/index.js
+++ Bar_della_fossa/src/index.js
@@ -1,12 +1,33 @@
-import _ from 'lodash';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase, ref, onValue} from "firebase/database";
+var $ = require('jquery');  //per jQuery
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA85vz6cMDje67KIxJzWCzpDABZmySzA5o",
+  authDomain: "bargabrach.firebaseapp.com",
+  databaseURL: "https://bargabrach-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "bargabrach",
+  storageBucket: "bargabrach.appspot.com",
+  messagingSenderId: "950724918886",
+  appId: "1:950724918886:web:ae6090c7bf363e1584b7a7",
+  measurementId: "G-KZ26VCVKN8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getDatabase();
+const prodottiRef = ref(db, 'prodotti');
+onValue(prodottiRef, (snapshot) => { //interfaccia per leggere dal database, trova i valori e li scarica
+  //snapshot.val() è la parte di database ad oggetti che leggo a valle di prodotti
+  //un oggetto in js può essere usato indefferentemente con la dot notation o come i dict (o mappe) in py
+  for(var k in snapshot.val()) {
+    $('#contenuto').append(snapshot.val()[k].nome+"<br>");
   }
-  
-  document.body.appendChild(component());
+});
