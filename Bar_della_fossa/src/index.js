350bae0d65c3f3df744161afd608e2f90e823c6d
Manu <82286334+Manuel-Russo@users.noreply.github.com>
2022-12-04 18:13:26 +0100

    tabella e bottoni con alert

--- Bar_della_fossa/src/index.js
+++ Bar_della_fossa/src/index.js
@@ -1,33 +1,34 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, onValue} from "firebase/database";
-var $ = require('jquery');  //per jQuery
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+var $ = require('jquery');  //per jQuery senò non le scarica
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA85vz6cMDje67KIxJzWCzpDABZmySzA5o",
-  authDomain: "bargabrach.firebaseapp.com",
-  databaseURL: "https://bargabrach-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "bargabrach",
-  storageBucket: "bargabrach.appspot.com",
-  messagingSenderId: "950724918886",
-  appId: "1:950724918886:web:ae6090c7bf363e1584b7a7",
-  measurementId: "G-KZ26VCVKN8"
+  apiKey: "AIzaSyCETuHC0yl4xJ59Ox6fm7IYO-2dvp1X17Q",
+  authDomain: "gabrach-25273.firebaseapp.com",
+  databaseURL: "https://gabrach-25273-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gabrach-25273",
+  storageBucket: "gabrach-25273.appspot.com",
+  messagingSenderId: "716936233864",
+  appId: "1:716936233864:web:5b494fbe9337b5e943b03d"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const db = getDatabase();
 const prodottiRef = ref(db, 'prodotti');
-onValue(prodottiRef, (snapshot) => { //interfaccia per leggere dal database, trova i valori e li scarica
-  //snapshot.val() è la parte di database ad oggetti che leggo a valle di prodotti
-  //un oggetto in js può essere usato indefferentemente con la dot notation o come i dict (o mappe) in py
-  for(var k in snapshot.val()) {
-    $('#contenuto').append(snapshot.val()[k].nome+"<br>");
+onValue(prodottiRef,(snapshot)=>{
+  for(var k in snapshot.val()){
+    //snapshot.val()=la parte di database a oggetti che leggo a valle di prodotti 
+    //un oggetto in javascript puo essere usato indifferentemente con la dot notation o come i dictionari di pyton infatti posso scrivere al posto k  in questo modo
+    //snapshot.val()[k][nome]
+    $('#contenuto').append("<tr>"+"<td>"+k+"</td>"+"<td>"+snapshot.val()[k].nome+"</td>"+"<td>" + "<button class='botAlert'>Schiaccia qui</button>" + "</td>"+"</tr>");
   }
+  $('.botAlert').onclick = function() { 
+    alert('Never Gonna Give You Up');
+  };
 });
+
+
